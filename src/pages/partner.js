c926fb3db605a67a2ab777394eb0cbb3db7bc560
Nghiêm Hoàng Đạt <nghiemhoangdat2001@gmail.com>
2024-04-21 11:33:15 +0700

    update

--- src/pages/partner.js
+++ src/pages/partner.js
@@ -25,22 +25,41 @@ function PartnerPage() {
           alert('Vui lòng điền đầy đủ thông tin.');
           return;
         }
-    
-        const response = await fetch(`${API_URL}/admin/partner`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(form),
-        });
 
-        if (response.status === 200) {
-            alert('Đăng ký thành công');
-            setShowForm(false);
-            handleLoadData();
+        if(form.id){
+            const response = await fetch(`${API_URL}/admin/partner/${form.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(form),
+            });
+    
+            if (response.status === 200) {
+                alert('Cập nhật thành công');
+                setShowForm(false);
+                handleLoadData();
+            } else {
+                alert('Cập nhật thất bại');
+            }
         } else {
-            alert('Đăng ký thất bại');
+            const response = await fetch(`${API_URL}/admin/partner`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(form),
+            });
+    
+            if (response.status === 200) {
+                alert('Đăng ký thành công');
+                setShowForm(false);
+                handleLoadData();
+            } else {
+                alert('Đăng ký thất bại');
+            }
         }
     };
 
@@ -51,6 +70,36 @@ function PartnerPage() {
           [name]: value,
         }));
     };
+
+    const handleUpdate = (item) => () => {
+        setForm(item);
+        setShowForm(true);
+    }
+
+    const handleAdd = () => {
+        setForm({
+            name: '',
+            address: '',
+            phone: '',
+        });
+        setShowForm(true);
+    }
+
+    const handleRemove = async (item) => {
+        const response = await fetch(`${API_URL}/admin/partner/${item.id}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              'Accept': 'application/json',
+            }
+        });
+        if (response.status === 200) {
+            alert('Xóa thành công');
+            handleLoadData();
+        } else {
+            alert('Xóa thất bại');
+        }
+    }
 
     const handleLoadData = async () => {
         const response = await fetch(`${API_URL}/admin/partner?per_page=${dataPerPage}&page=${currentPage}&name=${searchName}&date=${searchDate}`, {
@@ -96,7 +145,7 @@ function PartnerPage() {
                     <div>
                         <button 
                             className="bg-slate-400 border-slate-600 border-2 border-solid text-white py-2 px-4 rounded-md"
-                            onClick={() => setShowForm(true)}
+                            onClick={handleAdd}
                         >   
                             Add
                         </button>   
@@ -133,9 +182,8 @@ function PartnerPage() {
                         <td className="border px-4 py-2">{item.status}</td>
                         <td className="border px-4 py-2">
                             <div className="flex gap-4">
-                                <p className="text-blue-600">Detail</p>
-                                <p className="text-yellow-500">Edit</p>
-                                <p className="text-red-600">Delete</p>
+                                <p className="text-yellow-500" onClick={handleUpdate(item)}>Edit</p>
+                                <p className="text-red-600" onClick={() => handleRemove(item)}>Delete</p>
                             </div>
                         </td>
                       </tr>
@@ -197,12 +245,63 @@ function PartnerPage() {
                             onChange={handleFormChange}
                         />
                         </div>
+                        {
+                            form.id && (
+                                <div className="mb-4">
+                                    <label htmlFor="register_date" className="block text-gray-700 font-medium mb-1">
+                                        Ngày đăng ký
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="register_date"
+                                        name="register_date"
+                                        className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                        value={form.register_date}
+                                        onChange={handleFormChange}
+                                    />
+                                </div>
+                            )
+                        }
+                        {
+                            form.id && (
+                                <div className="mb-4">
+                                    <label htmlFor="discount" className="block text-gray-700 font-medium mb-1">
+                                        Chiết khấu
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="discount"
+                                        name="discount"
+                                        className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                        value={form.discount}
+                                        onChange={handleFormChange}
+                                    />
+                                </div>
+                            )
+                        }
+                        {
+                            form.id && (
+                                <div className="mb-4">
+                                    <label htmlFor="status" className="block text-gray-700 font-medium mb-1">
+                                        Trạng thái
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="status"
+                                        name="status"
+                                        className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                        value={form.status}
+                                        onChange={handleFormChange}
+                                    />
+                                </div>
+                            )
+                        }
                         <button
                         type="submit"
                         className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-md mb-4"
                         onClick={handleSubmit}
                         >
-                        Đăng ký
+                            {form.id ? 'Cập nhật' : 'Đăng ký'}
                         </button>
                         <button
                         className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-md"
